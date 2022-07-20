aaa24112da534ea7ea47381d8c50aac38bdc4d1e
linterobert <80642370+linterobert@users.noreply.github.com>
2022-07-20 16:15:21 +0300

    Add Controller

---
+++ WebApplication1/WebApplication1/TestCommit/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.TestCommit
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
